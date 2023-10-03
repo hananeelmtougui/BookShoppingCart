1052defe06eb16712c731d80247ef60d70fecac7
EL MTOUGUI Hanane <Hanane.EL-MTOUGUI@akkodis.com>
2023-10-03 17:08:38 +0100

    adding AuthorName field in the Book class and updating database

--- BookShoppingCartMvcUI/Models/Book.cs
+++ BookShoppingCartMvcUI/Models/Book.cs
@@ -11,6 +11,11 @@ namespace BookShoppingCartMvcUI.Models
         [Required]
         [MaxLength(40)]
         public string? BookName { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? AuthorName { get; set; }
+
         [Required]
         public double Price { get; set; }
         public string? Image {  get; set; }
